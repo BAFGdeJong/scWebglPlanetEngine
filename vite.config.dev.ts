a809c7b4ee98e09edda665b2d02d3e77f826dfad
Drajnoth <basdejong2002@live.nl>
2025-05-01 00:04:55 +0200

    Fixes

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    // root: '/',
     build: {
         outDir: 'dist',
         emptyOutDir: false,
@@ -16,6 +17,11 @@ export default defineConfig({
             output: {
                 inlineDynamicImports: true,
             },
+        },
+    },
+    server: {
+        watch: {
+            usePolling: true,
         },
     },
     // optimizeDeps: {
